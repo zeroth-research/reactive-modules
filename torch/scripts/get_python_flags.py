40a94f413933a63bfa117509330cd9a88b15e595
Marek Chalupa <marek.chalupa@zeroth.org>
2025-10-23 16:05:37 +0200

    Add basic Python bindings
    This is the basic Python code that allows to use
    our bindings from the side of Python.

--- torch/scripts/get_python_flags.py
+++ torch/scripts/get_python_flags.py
@@ -1,20 +1,21 @@
 from os.path import splitext
 import sysconfig
 
+
 def get_soname():
-    soname = sysconfig.get_config_vars('INSTSONAME') 
+    soname = sysconfig.get_config_vars("INSTSONAME")
     name, ext = splitext(soname[0])
-    if name.startswith('lib'):
+    if name.startswith("lib"):
         return name[3:]
     return name
 
-def get_sopath():
-    ldir = sysconfig.get_config_vars('LIBDIR') 
-    return ldir[0]
 
+def get_sopath():
+    ldir = sysconfig.get_config_vars("LIBDIR")
+    return ldir[0]
 
 
 if __name__ == "__main__":
 
-    print(f"cargo:rustc-link-search={get_sopath()}");
-    print(f"cargo:rustc-link-lib={get_soname()}");
+    print(f"cargo:rustc-link-search={get_sopath()}")
+    print(f"cargo:rustc-link-lib={get_soname()}")
