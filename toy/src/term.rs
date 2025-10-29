3dc8f4ff5a6c1dac1f8259b77b35afad525feb1c
Marek Chalupa <marek.chalupa@zeroth.org>
2025-10-29 17:07:59 +0100

    toy: implement the example

--- toy/src/term.rs
+++ toy/src/term.rs
@@ -2,5 +2,108 @@ use base::term::Term as BaseTerm;
 
 use crate::instruction::Instruction;
 use crate::val::Type;
+use base::wire::Wire;
 
 pub type Term = BaseTerm<Type, Instruction>;
+
+pub fn construct(
+    ins: Instruction,
+    writes: Wire<Type>,
+    reads: Wire<Type>,
+) -> Result<Term, &'static str> {
+    match ins {
+        Instruction::Const(val) => {
+            if !reads.is_empty() {
+                return Err("Const should read nothing");
+            }
+            if writes.size() != 1 {
+                return Err("Const must write to exactly one wire");
+            }
+            let out = writes.iter().next().unwrap();
+            if !val.same_type(*out.1) {
+                return Err("Const writes to incompatible wire");
+            }
+        }
+        Instruction::Eq | Instruction::Lt => {
+            if !reads.size() == 2 {
+                return Err("Comparison must read two values");
+            }
+            if !writes.size() == 1 {
+                return Err("Comparison must write 1 value");
+            }
+            let out = writes.iter().next().unwrap();
+            if *out.1 != Type::Bool {
+                return Err("Comparison does not write bool");
+            }
+        }
+        Instruction::Or => {
+            if !reads.size() == 2 {
+                return Err("Comparison must read two values");
+            }
+            if !writes.size() == 1 {
+                return Err("Comparison must write 1 value");
+            }
+            let out = writes.iter().next().unwrap();
+            if *out.1 != Type::Bool {
+                return Err("Comparison does not write bool");
+            }
+            for (_, ty) in reads.iter() {
+                if *ty != Type::Bool {
+                    return Err("Input to Or must be a boolean");
+                }
+            }
+        }
+        Instruction::Id => {
+            if !reads.size() == 1 {
+                return Err("Id must read one wire");
+            }
+            if !writes.size() == 1 {
+                return Err("Id must write one wire");
+            }
+            let in_ = reads.iter().next().unwrap();
+            let out_ = writes.iter().next().unwrap();
+            if *in_.1 != *out_.1 {
+                return Err("Id reads and writes different types");
+            }
+        }
+        Instruction::Sum => {
+            if !reads.size() == 2 {
+                return Err("Sum must read two wires");
+            }
+            if !writes.size() == 1 {
+                return Err("Sum must write one wire");
+            }
+            let out_ = writes.iter().next().unwrap();
+            if *out_.1 == Type::Bool {
+                return Err("Sum writes Bool");
+            }
+            for (_, ty) in reads.iter() {
+                if *ty != *out_.1 {
+                    return Err("Sum reads and writes different types");
+                }
+            }
+        }
+        Instruction::Ite => {
+            if !reads.size() == 3 {
+                return Err("Ite must read three wires");
+            }
+            if !writes.size() == 1 {
+                return Err("Ite must write one wire");
+            }
+            let out_ty = writes.iter().next().unwrap().1;
+            let types: Vec<&Type> = reads.iter().map(|(_, ty)| ty).collect();
+
+            if *types[0] != Type::Bool {
+                return Err("Ite first argument must be Bool");
+            }
+            if *types[1] != *types[2] {
+                return Err("Ite second and thrid arguments must have the same type");
+            }
+            if *out_ty != *types[1] {
+                return Err("Ite must write the same type as the second and third arguments");
+            }
+        }
+    }
+
+    Ok(Term::new(ins, writes, reads))
+}
