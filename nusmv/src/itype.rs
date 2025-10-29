c6828ce9d2f66e811f110205ed9c44aee79b3dcf
Marco Casadio <marco@Marcos-MacBook-Pro.local>
2025-10-29 08:53:26 +0000

    updating the nusmv parser

--- nusmv/src/itype.rs
+++ nusmv/src/itype.rs
@@ -1,9 +1,8 @@
-use base::term::Instruction;
-
 #[derive(Clone, Debug)]
 pub enum IType {
     // Values and variables
     ConstBool(bool),
+    ConstInt(i64),
     VarRef(String),
 
     // Boolean operations
@@ -15,19 +14,4 @@ pub enum IType {
     Next(Box<IType>),
     Init(Box<IType>),
     Assign(Box<IType>, Box<IType>),
-}
-
-impl Instruction for IType {
-    fn arity(&self) -> Option<usize> {
-        match self {
-            IType::ConstBool(_) => Some(0),
-            IType::VarRef(_) => Some(0),
-            IType::Not(_) => Some(1),
-            IType::And(_, _) => Some(2),
-            IType::Or(_, _) => Some(2),
-            IType::Next(_) => Some(1),
-            IType::Init(_) => Some(1),
-            IType::Assign(_, _) => Some(2),
-        }
-    }
 }
