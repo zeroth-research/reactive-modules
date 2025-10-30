13ada6acd65102be96ff4fc63aae6299c43bf641
Marek Chalupa <marek.chalupa@zeroth.org>
2025-10-30 12:17:26 +0100

    toy/term: use ref in same_type

--- toy/src/val.rs
+++ toy/src/val.rs
@@ -17,13 +17,13 @@ pub enum Val {
 }
 
 impl Val {
-    pub fn same_type(&self, ty: Type) -> bool {
+    pub fn same_type(&self, ty: &Type) -> bool {
         match self {
-            Val::Real(_) => ty == Type::Real,
-            Val::NReal(_) => ty == Type::NReal,
-            Val::Int(_) => ty == Type::Int,
-            Val::NInt(_) => ty == Type::NInt,
-            Val::Bool(_) => ty == Type::Bool,
+            Val::Real(_) => *ty == Type::Real,
+            Val::NReal(_) => *ty == Type::NReal,
+            Val::Int(_) => *ty == Type::Int,
+            Val::NInt(_) => *ty == Type::NInt,
+            Val::Bool(_) => *ty == Type::Bool,
         }
     }
 }
