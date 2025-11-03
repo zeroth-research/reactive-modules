a4e2c2cda08b82f61d44aaf8cba8e2fccce62e20
Marek Chalupa <marek.chalupa@zeroth.org>
2025-11-03 22:39:16 +0100

    toy: add skeleton for interpreter

--- toy/src/interpreter/mod.rs
+++ toy/src/interpreter/mod.rs
@@ -1,4 +1,8 @@
 mod eval;
+mod interpreter;
 mod state;
 
 pub use eval::eval;
+
+pub use interpreter::Interpreter;
+pub use state::State;
---
+++ toy/tests/interpreter.rs
@@ -0,0 +1,13 @@
+use toy::instruction::{ArithOp, Instruction};
+use toy::interpreter::{Interpreter, eval};
+use toy::val::Val;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn interpreter_1() {
+        let I = Interpreter::new();
+    }
+}
