f8ca7277595e51cc5d79319dea6356df9db90129
Marek Chalupa <marek.chalupa@zeroth.org>
2025-11-06 10:30:51 +0100

    toy: implement `Display` for the important structs/enums

--- toy/src/val.rs
+++ toy/src/val.rs
@@ -1,4 +1,5 @@
 use crate::dtype::Type;
+use std::fmt;
 
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
 pub enum Val {
@@ -110,5 +111,18 @@ impl Val {
         }
 
         None
+    }
+}
+
+impl fmt::Display for Val {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Val::None => write!(f, "None"),
+            Val::Real(x) => write!(f, "Real({})", x),
+            Val::NReal(x) => write!(f, "NReal({})", x),
+            Val::Int(x) => write!(f, "Int({})", x),
+            Val::NInt(x) => write!(f, "NInt({})", x),
+            Val::Bool(x) => write!(f, "Bool({})", x),
+        }
     }
 }
