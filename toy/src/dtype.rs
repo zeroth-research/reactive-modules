0b2d452c64f69dc2716d3a2f39b720f5b92c5b03
Marek Chalupa <marek.chalupa@zeroth.org>
2025-11-06 10:30:51 +0100

    toy: implement `Display` for the important structs/enums

--- toy/src/dtype.rs
+++ toy/src/dtype.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
 pub enum Type {
     Real,
@@ -16,6 +18,18 @@ impl Type {
             "NInt" => Ok(Type::NInt),
             "Bool" => Ok(Type::Bool),
             _ => Err("Invalid Type (cannot convert from this str)"),
+        }
+    }
+}
+
+impl fmt::Display for Type {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Type::Real => write!(f, "Real"),
+            Type::NReal => write!(f, "NReal"),
+            Type::Int => write!(f, "Int"),
+            Type::NInt => write!(f, "NInt"),
+            Type::Bool => write!(f, "Bool"),
         }
     }
 }
