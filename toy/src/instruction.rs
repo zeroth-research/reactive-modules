e394f6c3e8093cce6095f915aee7d152abf32bd6
Marek Chalupa <marek.chalupa@zeroth.org>
2025-11-06 10:30:51 +0100

    toy: implement `Display` for the important structs/enums

--- toy/src/instruction.rs
+++ toy/src/instruction.rs
@@ -1,4 +1,5 @@
 use crate::val::Val;
+use std::fmt;
 
 #[derive(Debug, Copy, Clone)]
 pub enum CmpOp {
@@ -34,4 +35,48 @@ pub enum Instruction {
     Ite,
     // arith
     Arith(ArithOp),
+}
+
+impl fmt::Display for CmpOp {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            CmpOp::Eq => write!(f, "Eq"),
+            CmpOp::Lt => write!(f, "Lt"),
+            CmpOp::Le => write!(f, "Le"),
+        }
+    }
+}
+
+impl fmt::Display for LogicalOp {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            LogicalOp::And => write!(f, "And"),
+            LogicalOp::Or => write!(f, "Or"),
+            LogicalOp::Not => write!(f, "Not"),
+        }
+    }
+}
+
+impl fmt::Display for ArithOp {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ArithOp::Add => write!(f, "Add"),
+            ArithOp::Sub => write!(f, "Sub"),
+            ArithOp::Mul => write!(f, "Mul"),
+            ArithOp::Div => write!(f, "Div"),
+        }
+    }
+}
+
+impl fmt::Display for Instruction {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Instruction::Const(v) => write!(f, "Const({})", v),
+            Instruction::Id => write!(f, "Id"),
+            Instruction::Logical(op) => write!(f, "Logical::{}", op),
+            Instruction::Arith(op) => write!(f, "Arith::{}", op),
+            Instruction::Cmp(op) => write!(f, "Cmp::{}", op),
+            Instruction::Ite => write!(f, "Ite"),
+        }
+    }
 }
