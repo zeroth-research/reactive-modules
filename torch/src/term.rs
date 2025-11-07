479fbdcc9aec36eb1cf99fac173fb88713513bea
Marek Chalupa <marek.chalupa@zeroth.org>
2025-11-07 15:02:26 +0100

    torch: implement creating modules

--- torch/src/term.rs
+++ torch/src/term.rs
@@ -1,4 +1,5 @@
 use base::term::Term;
+use std::fmt;
 
 #[derive(Debug)]
 pub enum TorchOp {
@@ -17,14 +18,20 @@ pub enum TorchOp {
     Mul,
     Sub,
     Div,
-    // Guard
-    Guard,
-    // either boolean negation or negating a number
-    // (depending on the type of inputs)
+    // If-then-else
+    Ite,
+    // Boolean terms
+    //
     Neg,
+    Or,
+    And,
 }
 
-pub type TorchDType = &'static str;
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum TorchDType {
+    Tensor,
+}
+
 pub type TorchTerm = Term<TorchDType, TorchOp>;
 
 impl TorchOp {
@@ -43,8 +50,11 @@ impl TorchOp {
             "Div" => TorchOp::Div,
             "Sum" => TorchOp::Sum,
             // -----
-            "Guard" => TorchOp::Guard,
+            "Ite" => TorchOp::Ite,
+            // -----
             "Neg" => TorchOp::Neg,
+            "Or" => TorchOp::Or,
+            "And" => TorchOp::And,
             // -----
             "Const" => panic!("Const cannot be constructed from a &str"),
             oth => panic!("Invalid TorchOp: {} (maybe just not added yet)", oth),
@@ -55,6 +65,7 @@ impl TorchOp {
 impl Clone for TorchOp {
     fn clone(&self) -> Self {
         match self {
+            TorchOp::Const(v) => Self::Const(v.shallow_clone()),
             TorchOp::Eq => TorchOp::Eq,
             TorchOp::Neq => TorchOp::Neq,
             TorchOp::Lt => TorchOp::Lt,
@@ -66,9 +77,41 @@ impl Clone for TorchOp {
             TorchOp::Mul => TorchOp::Mul,
             TorchOp::Div => TorchOp::Div,
             TorchOp::Sum => TorchOp::Sum,
-            TorchOp::Guard => TorchOp::Guard,
+            TorchOp::Ite => TorchOp::Ite,
             TorchOp::Neg => TorchOp::Neg,
-            TorchOp::Const(v) => Self::Const(v.shallow_clone()),
+            TorchOp::And => TorchOp::And,
+            TorchOp::Or => TorchOp::Or,
+        }
+    }
+}
+
+impl fmt::Display for TorchOp {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            TorchOp::Eq => write!(f, "Eq"),
+            TorchOp::Neq => write!(f, "Neq"),
+            TorchOp::Lt => write!(f, "Lt"),
+            TorchOp::Le => write!(f, "Le"),
+            TorchOp::Gt => write!(f, "Gt"),
+            TorchOp::Ge => write!(f, "Ge"),
+            TorchOp::Add => write!(f, "Add"),
+            TorchOp::Sub => write!(f, "Sub"),
+            TorchOp::Mul => write!(f, "Mul"),
+            TorchOp::Div => write!(f, "Div"),
+            TorchOp::Sum => write!(f, "Sum"),
+            TorchOp::Ite => write!(f, "Ite"),
+            TorchOp::Neg => write!(f, "Neg"),
+            TorchOp::And => write!(f, "And"),
+            TorchOp::Or => write!(f, "Or"),
+            TorchOp::Const(v) => write!(f, "Const({})", v),
+        }
+    }
+}
+
+impl fmt::Display for TorchDType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            TorchDType::Tensor => write!(f, "TorchDType::Tensor"),
         }
     }
 }
