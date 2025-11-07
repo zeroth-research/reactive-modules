12289bdefb1c1316709972b16e328529340cf15f
Marek Chalupa <marek.chalupa@zeroth.org>
2025-11-07 15:02:26 +0100

    torch: implement creating modules

--- torch/python/bindings/term.py
+++ torch/python/bindings/term.py
@@ -4,16 +4,16 @@ from . import libzrm_torch
 Context = libzrm_torch.Context
 
 PyVal = libzrm_torch.PyVal
-PyTerm = libzrm_torch.PyTerm
+WrappedTerm = libzrm_torch.WrappedTerm
 
 
-def to_pyval(v):
+def to_pyval(v) -> PyVal:
     if isinstance(v, PyVal):
         return v
     if isinstance(v, Var):
         return v.term_
     if isinstance(v, str):  # this is a wire symbol
-        return Var(v)
+        return Var(v).term_
     return PyVal(v)
 
 
@@ -38,13 +38,16 @@ class Term:
         reads = [to_pyval(r) for r in reads]
         writes = [to_pyval(outvar)]
 
-        self.term_ = PyTerm(op, reads, writes)
-        self.outvar = outvar
+        self.term_: WrappedTerm = WrappedTerm(op, reads, writes)
+        self.outvar: Var = outvar
 
         # libzrm_torch.print_pyterm(self.term_)
 
     def print(self):
         self.term_.print()
+
+    def wrapped_term(self) -> WrappedTerm:
+        return self.term_
 
     def __str__(self) -> str:
         return str(self.term_)
@@ -52,7 +55,7 @@ class Term:
     def __repr__(self) -> str:
         return f"Term({self.term_})"
 
-    def _op(self, op, arg):
+    def _op(self, op, arg) -> "Term":
         if isinstance(arg, (int, float, TorchTensor, Var)):
             term = self.ctx_.term(op, [self.outvar, arg])
             return term.outvar
@@ -60,7 +63,7 @@ class Term:
             f"Unsupported type for operation: {arg} ({type(arg)})"
         )
 
-    def _r_op(self, op, arg):
+    def _r_op(self, op, arg) -> "Term":
         if isinstance(arg, (int, float, TorchTensor, Var)):
             term = self.ctx_.term(op, [arg, self.outvar])
             return term.outvar
@@ -80,14 +83,34 @@ class Term:
     def __rmul__(self, rhs):
         return self._r_op("Mul", rhs)
 
+    def __lt__(self, rhs):
+        return self._op("Lt", rhs)
+
+    def __rlt__(self, rhs):
+        return self._r_op("Lt", rhs)
+
+    # TODO: other relational operators
+
+    # logical or
+    def __or__(self, rhs: "Term") -> "Term":
+        return self.ctx_.term("Or", [self.outvar, rhs.outvar]).outvar
+
+    # logical and
+    def __and__(self, rhs: "Term") -> "Term":
+        return self.ctx_.term("And", [self.outvar, rhs.outvar]).outvar
+
+    # logical negation: ~
+    def __invert__(self) -> "Term":
+        return self.neg()
+
     # do not convert terms to 0 or 1
     def __bool__(self):
         raise NotImplementedError("Cannot convert term to bool")
 
-    def neg(self):
+    def neg(self) -> "Term":
         return self.ctx_.term("Neg", [self.outvar]).outvar
 
-    def sum(self):
+    def sum(self) -> "Term":
         return self.ctx_.term("Sum", [self.outvar]).outvar
 
 
@@ -96,14 +119,48 @@ class Var(Term):
     NOTE: This constructor should be called only by Context.
     """
 
-    def __init__(self, ctx, name, id_=None):
+    def __init__(self, ctx: Context, name: str, id_=None):
         super().__init__(ctx, "Var", [])
-        self.name = name
+        self.name: str = name
 
         # below I set the attributes from the parent class directly,
         # I'll burn in hell for that.
-        self.outvar = self
-        # The underlying PyTerm
+        self.outvar: Var = self
+        # The underlying WrappedTerm
         if id_ is None:
             id_ = ctx.get_var_id(name)
-        self.term_ = PyVal.sym(id_)
+        self.term_: PyVal = PyVal.sym(id_)
+
+
+class Assignment:
+    def __init__(self, lhs: "NextVar", rhs: Term):
+        self.var = lhs
+        self.rhs = rhs
+
+    def __str__(self):
+        return f"{self.var} := {self.rhs}"
+
+
+class NextVar(Var):
+    """
+    A class for primed variables. For these, we want to override some operators
+    differently. Otherwise, they are the same as Var.
+
+    :param: name  The name of **current state** variable.
+    """
+
+    def __init__(self, ctx: Context, name: str):
+        assert isinstance(name, str), (name, type(name))
+        super().__init__(ctx, f"{name}'")
+        # we store also the current variable name, so that we can easily
+        # get the latched variable from this one
+        self.latched_name: str = name
+
+    def get_latched(self) -> Var:
+        return self.ctx_.var(self.latched_name)
+
+    def __eq__(self, rhs: Term) -> Assignment:
+        """
+        Operator == works as assignment for primed variables.
+        """
+        return Assignment(self, rhs)
--- torch/src/wrappedatom.rs
+++ torch/src/wrappedatom.rs
@@ -15,7 +15,7 @@ type WireTy = Wire<TorchDType>;
 
 #[pyclass]
 pub struct WrappedAtom {
-    atom: Atom<TorchDType, TorchOp>,
+    pub(crate) atom: Atom<TorchDType, TorchOp>,
 }
 
 // It is safe to share this struct for the same reasons as for PyTensor
@@ -57,7 +57,7 @@ fn process_pyvals(
                 let var = ctx.fresh_var();
                 result.push(TorchTerm::new(
                     TorchOp::Const(tensor.tensor.shallow_clone()),
-                    Wire::one(var, "tensor"),
+                    Wire::one(var, TorchDType::Tensor),
                     Wire::none(),
                 ));
                 args.push(var);
@@ -66,7 +66,7 @@ fn process_pyvals(
                 let var = ctx.fresh_var();
                 result.push(TorchTerm::new(
                     TorchOp::Const(tch::Tensor::from_slice(&[*val])),
-                    Wire::one(var, "tensor"),
+                    Wire::one(var, TorchDType::Tensor),
                     Wire::none(),
                 ));
                 args.push(var);
@@ -75,7 +75,7 @@ fn process_pyvals(
                 let var = ctx.fresh_var();
                 result.push(TorchTerm::new(
                     TorchOp::Const(tch::Tensor::from_slice(&[*val])),
-                    Wire::one(var, "tensor"),
+                    Wire::one(var, TorchDType::Tensor),
                     Wire::none(),
                 ));
                 args.push(var);
@@ -108,8 +108,8 @@ fn wterms_to_torchterms(ctx: &mut Context, terms: &Bound<'_, PyList>) -> PyResul
         let rargs = process_pyvals(ctx, &wterm.reads, &mut result);
         let wargs = process_pyvals(ctx, &wterm.writes, &mut result);
 
-        let write = Wire::from_iter(wargs.into_iter().map(|val| (val, "tensor")));
-        let read = Wire::from_iter(rargs.into_iter().map(|val| (val, "tensor")));
+        let write = Wire::from_iter(wargs.into_iter().map(|val| (val, TorchDType::Tensor)));
+        let read = Wire::from_iter(rargs.into_iter().map(|val| (val, TorchDType::Tensor)));
         result.push(TorchTerm::new(wterm.op.clone(), write, read));
     }
 
@@ -118,7 +118,7 @@ fn wterms_to_torchterms(ctx: &mut Context, terms: &Bound<'_, PyList>) -> PyResul
 
 /// Translate a list of `PyVal::Sym` values into a wiring. This is a simple version of
 /// [process_pyvals] where we assume no constants are in the input vector.
-fn vars_to_wiring(vals: &Bound<'_, PyList>) -> PyResult<WireTy> {
+pub(crate) fn vars_to_wiring(vals: &Bound<'_, PyList>) -> PyResult<WireTy> {
     let mut names: Vec<usize> = Vec::new();
     for item in vals {
         let pyval: &Bound<'_, PyVal> = item.downcast()?;
@@ -130,51 +130,52 @@ fn vars_to_wiring(vals: &Bound<'_, PyList>) -> PyResult<WireTy> {
     }
 
     Ok(Wire::from_iter(
-        names.into_iter().map(|val| (val, "tensor")),
+        names.into_iter().map(|val| (val, TorchDType::Tensor)),
     ))
 }
 
 #[pymethods]
 impl WrappedAtom {
+    //fn new_unchecked(
+    //    ctx: &Bound<'_, Context>,
+    //    read: &Bound<'_, PyList>,
+    //    ctrl: &Bound<'_, PyList>,
+    //    init: &Bound<'_, PyList>,
+    //    update: &Bound<'_, PyList>,
+    //) -> Self {
+    //    let read = vars_to_wiring(read).unwrap();
+    //    let ctrl = vars_to_wiring(ctrl).unwrap();
+    //
+    //    let ctx: &mut Context = &mut ctx.borrow_mut();
+    //    let init = wterms_to_torchterms(ctx, init).unwrap();
+    //    let update = wterms_to_torchterms(ctx, update).unwrap();
+    //
+    //    let atom = Atom::new_unchecked(read, ctrl, Wire::none(), init, update);
+    //    Self { atom }
+    //}
+
     #[new]
     fn new(
         ctx: &Bound<'_, Context>,
-        read: &Bound<'_, PyList>,
-        write: &Bound<'_, PyList>,
+        // variables
+        latched: &Bound<'_, PyList>,
+        next: &Bound<'_, PyList>,
+        // init and update
         init: &Bound<'_, PyList>,
         update: &Bound<'_, PyList>,
     ) -> Self {
-        let read = vars_to_wiring(read).unwrap();
-        let write = vars_to_wiring(write).unwrap();
+        let latched = vars_to_wiring(latched).unwrap();
+        let next = vars_to_wiring(next).unwrap();
 
         let ctx: &mut Context = &mut ctx.borrow_mut();
         let init = wterms_to_torchterms(ctx, init).unwrap();
         let update = wterms_to_torchterms(ctx, update).unwrap();
 
-        let atom = Atom::new_unchecked(read, write, Wire::none(), init, update);
+        let atom = Atom::with_module_wire(&[latched, next], init, update).unwrap();
         Self { atom }
     }
 
     fn dbg(&self) {
-        println!("{:?}", self);
-    }
-}
-
-impl std::fmt::Debug for WrappedAtom {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        writeln!(f, "Reads: {:?}", self.atom.read())?;
-        writeln!(f, "Writes: {:?}", self.atom.ctrl())?;
-        writeln!(f, "Awaits: NOT IMPLEMENTED {:?}", self.atom.wait())?;
-        writeln!(f, "-------------")?;
-        writeln!(f, "Init:")?;
-        for term in self.atom.init() {
-            writeln!(f, "  {:?}", term)?;
-        }
-        writeln!(f, "-------------")?;
-        writeln!(f, "Update:")?;
-        for term in self.atom.update() {
-            writeln!(f, "  {:?}", term)?;
-        }
-        writeln!(f, "-------------")
+        println!("{}", self.atom);
     }
 }
