0383617de66602e76a1a8f953f4db80cd9d7cb35
Marek Chalupa <marek.chalupa@zeroth.org>
2025-11-07 14:31:13 +0100

    Replace Py{Atom,Term} with Wrapped{Atom,Term}
    The name fits better and we avoid confustion with things like
    `PyModule` which are PyO3 structs.

--- torch/src/lib.rs
+++ torch/src/lib.rs
@@ -1,29 +1,28 @@
 mod context;
-mod pyatom;
 mod pytensor;
-mod pyterm;
 mod pyval;
 mod term;
+mod wrappedatom;
+mod wrappedterm;
 
 use pyo3::prelude::*;
-use pyterm::print_pyterm;
 
 pub use context::Context;
-pub use pyatom::PyAtom;
 pub use pytensor::PyTensor;
-pub use pyterm::PyTerm;
 pub use pyval::PyVal;
+pub use wrappedatom::WrappedAtom;
+pub use wrappedterm::WrappedTerm;
 
 #[pymodule]
 #[pyo3(name = "zrm_torch")]
 fn zrm_torch(py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
     //#[pymodule_export]
     m.add_class::<PyVal>()?;
-    m.add_class::<PyTerm>()?;
-    m.add_class::<PyAtom>()?;
+    m.add_class::<WrappedTerm>()?;
+    m.add_class::<WrappedAtom>()?;
     m.add_class::<Context>()?;
 
     py.import("torch")?;
-    m.add_function(wrap_pyfunction!(print_pyterm, m)?)?;
+    //m.add_function(wrap_pyfunction!(print_pyterm, m)?)?;
     Ok(())
 }
