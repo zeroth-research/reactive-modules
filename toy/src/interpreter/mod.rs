fffcea4358d5304884d6fa3e06debaaeca21065a
Marek Chalupa <marek.chalupa@zeroth.org>
2025-11-19 15:21:09 +0100

    toy: rename modules to make clippy happy

--- toy/src/interpreter/mod.rs
+++ toy/src/interpreter/mod.rs
@@ -1,8 +1,34 @@
 mod eval;
-mod interpreter;
 mod state;
 
 pub use eval::eval;
 
-pub use interpreter::Interpreter;
 pub use state::State;
+
+use crate::dtype::Type;
+use crate::instruction::Instruction;
+use base::module::Module;
+
+pub struct Interpreter<'a> {
+    module: &'a Module<Type, Instruction>,
+    state: State,
+}
+
+impl<'a> Interpreter<'a> {
+    pub fn new(module: &'a Module<Type, Instruction>) -> Self {
+        Self {
+            module,
+            state: State::new(),
+        }
+    }
+
+    /// initialize the execution state
+    fn initialize(&mut self) -> Result<(), &'static str> {
+        unimplemented!()
+    }
+
+    /// take the next step in execution
+    fn step(&mut self) -> Result<(), &'static str> {
+        unimplemented!()
+    }
+}
