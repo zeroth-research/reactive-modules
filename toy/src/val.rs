ff2b4d557c4bd555352fb1f006a5aa0e5e64341b
Marek Chalupa <marek.chalupa@zeroth.org>
2025-11-24 17:11:56 +0100

    toy: remove NInt and NReal
    We don't use these at all

--- toy/src/val.rs
+++ toy/src/val.rs
@@ -5,9 +5,7 @@ use std::fmt;
 pub enum Val {
     None, // no value
     Real(f64),
-    NReal(f64),
     Int(i64),
-    NInt(u64),
     Bool(bool),
 }
 
@@ -21,9 +19,7 @@ impl Val {
     pub(crate) fn add(&self, rhs: &Val) -> Option<Self> {
         match (self, rhs) {
             (Val::Real(x), Val::Real(y)) => Some(Val::Real(x + y)),
-            (Val::NReal(x), Val::NReal(y)) => Some(Val::NReal(x + y)),
             (Val::Int(x), Val::Int(y)) => Some(Val::Int(x + y)),
-            (Val::NInt(x), Val::NInt(y)) => Some(Val::NInt(x + y)),
             _ => None,
         }
     }
@@ -31,9 +27,7 @@ impl Val {
     pub(crate) fn sub(&self, rhs: &Val) -> Option<Self> {
         match (self, rhs) {
             (Val::Real(x), Val::Real(y)) => Some(Val::Real(x - y)),
-            (Val::NReal(x), Val::NReal(y)) => Some(Val::NReal(x - y)),
             (Val::Int(x), Val::Int(y)) => Some(Val::Int(x - y)),
-            (Val::NInt(x), Val::NInt(y)) => Some(Val::NInt(x - y)),
             _ => None,
         }
     }
@@ -41,9 +35,7 @@ impl Val {
     pub(crate) fn mul(&self, rhs: &Val) -> Option<Self> {
         match (self, rhs) {
             (Val::Real(x), Val::Real(y)) => Some(Val::Real(x * y)),
-            (Val::NReal(x), Val::NReal(y)) => Some(Val::NReal(x * y)),
             (Val::Int(x), Val::Int(y)) => Some(Val::Int(x * y)),
-            (Val::NInt(x), Val::NInt(y)) => Some(Val::NInt(x * y)),
             _ => None,
         }
     }
@@ -51,9 +43,7 @@ impl Val {
     pub(crate) fn div(&self, rhs: &Val) -> Option<Self> {
         match (self, rhs) {
             (Val::Real(x), Val::Real(y)) => Some(Val::Real(x / y)),
-            (Val::NReal(x), Val::NReal(y)) => Some(Val::NReal(x / y)),
             (Val::Int(x), Val::Int(y)) => Some(Val::Int(x / y)),
-            (Val::NInt(x), Val::NInt(y)) => Some(Val::NInt(x / y)),
             _ => None,
         }
     }
@@ -62,9 +52,7 @@ impl Val {
         match self {
             Val::None => false,
             Val::Real(_) => *ty == Type::Real,
-            Val::NReal(_) => *ty == Type::NReal,
             Val::Int(_) => *ty == Type::Int,
-            Val::NInt(_) => *ty == Type::NInt,
             Val::Bool(_) => *ty == Type::Bool,
         }
     }
@@ -73,9 +61,7 @@ impl Val {
         match self {
             Val::None => matches!(rhs, Val::None),
             Val::Real(_) => matches!(rhs, Val::Real { .. }),
-            Val::NReal(_) => matches!(rhs, Val::NReal { .. }),
             Val::Int(_) => matches!(rhs, Val::Int { .. }),
-            Val::NInt(_) => matches!(rhs, Val::NInt { .. }),
             Val::Bool(_) => matches!(rhs, Val::Bool { .. }),
         }
     }
@@ -92,19 +78,9 @@ impl Val {
                     return Some(Val::Int(val));
                 }
             }
-            Type::NInt => {
-                if let Ok(val) = val.parse() {
-                    return Some(Val::NInt(val));
-                }
-            }
             Type::Real => {
                 if let Ok(val) = val.parse() {
                     return Some(Val::Real(val));
-                }
-            }
-            Type::NReal => {
-                if let Ok(val) = val.parse() {
-                    return Some(Val::NReal(val));
                 }
             }
         }
@@ -124,9 +100,7 @@ impl fmt::Display for Val {
         match self {
             Val::None => write!(f, "None"),
             Val::Real(x) => write!(f, "Real({})", x),
-            Val::NReal(x) => write!(f, "NReal({})", x),
             Val::Int(x) => write!(f, "Int({})", x),
-            Val::NInt(x) => write!(f, "NInt({})", x),
             Val::Bool(x) => write!(f, "Bool({})", x),
         }
     }
