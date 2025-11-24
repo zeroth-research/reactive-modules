570d43c92afd8024e17938d7712f3eb9f8474ee0
Marek Chalupa <marek.chalupa@zeroth.org>
2025-11-24 17:11:56 +0100

    toy: remove NInt and NReal
    We don't use these at all

--- toy/src/dtype.rs
+++ toy/src/dtype.rs
@@ -3,9 +3,7 @@ use std::fmt;
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
 pub enum Type {
     Real,
-    NReal,
     Int,
-    NInt,
     Bool,
 }
 
@@ -15,9 +13,7 @@ impl std::str::FromStr for Type {
     fn from_str(ty: &str) -> Result<Self, Self::Err> {
         match ty {
             "Real" => Ok(Type::Real),
-            "NReal" => Ok(Type::NReal),
             "Int" => Ok(Type::Int),
-            "NInt" => Ok(Type::NInt),
             "Bool" => Ok(Type::Bool),
             _ => Err("Invalid Type (cannot convert from this str)"),
         }
@@ -28,9 +24,7 @@ impl fmt::Display for Type {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Type::Real => write!(f, "Real"),
-            Type::NReal => write!(f, "NReal"),
             Type::Int => write!(f, "Int"),
-            Type::NInt => write!(f, "NInt"),
             Type::Bool => write!(f, "Bool"),
         }
     }
