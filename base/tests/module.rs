c62a8692be37a78940071169dad5232e03c497f6
Mirco Giacobbe <mirco.giacobbe@gmail.com>
2025-12-01 00:46:54 +0000

    Added Module::parallel

--- base/tests/module.rs
+++ base/tests/module.rs
@@ -54,58 +54,8 @@ fn example_counter() -> Result<Module<&'static str, &'static str>, &'static str>
     Module::sequential(obs, init, update)
 }
 
-#[test]
-fn can_instantiate_sequential_module() {
-    let _module = example_counter().unwrap();
-    //print!("{}", _module);
-}
-
-#[test]
-fn can_instantiate_partially_observable_module() {
-    let m = example_counter().unwrap();
-    let wires = m.obs().clone();
-    let mut obs: Vec<[Wire<&'static str>; 2]> = Vec::new();
-    let mut prvt: Vec<[Wire<&'static str>; 2]> = Vec::new();
-    for [ltc, nxt] in wires {
-        if ltc.id() == 0 {
-            prvt.push([ltc, nxt]);
-        } else {
-            obs.push([ltc, nxt]);
-        }
-    }
-    let obs = Interface::from_iter(obs);
-    let prvt = Interface::from_iter(prvt);
-
-    let m = Module::partially_observable(obs, prvt, m.atoms().iter().cloned());
-    //print!("{}", m);
-    assert!(m.is_ok());
-}
-
-#[test]
-fn cannot_instantiate_external_unobservable_wire() {
-    let m = example_counter().unwrap();
-    let wires = m.obs().clone();
-    let mut obs: Vec<[Wire<&'static str>; 2]> = Vec::new();
-    let mut prvt: Vec<[Wire<&'static str>; 2]> = Vec::new();
-    for [ltc, nxt] in wires {
-        if ltc.id() == 3 {
-            prvt.push([ltc, nxt]);
-        } else {
-            obs.push([ltc, nxt]);
-        }
-    }
-    let obs = Interface::from_iter(obs);
-    let prvt = Interface::from_iter(prvt);
-
-    let m = Module::partially_observable(obs, prvt, m.atoms().iter().cloned());
-    //print!("{}", m);
-
-    assert!(m.is_err());
-}
-
-#[test]
 #[allow(clippy::vec_init_then_push)]
-fn example_peterson1() {
+fn example_peterson1() -> Result<Module<&'static str, &'static str>, &'static str> {
     let stype = "{outCS, reqCS, inCS}";
     let pc1: [Wire<&str>; 2] = [(0, stype), (1, stype)].map(Into::into);
     let x1: [Wire<&str>; 2] = [(2, "bool"), (3, "bool")].map(Into::into);
@@ -163,7 +113,98 @@ fn example_peterson1() {
     );
 
     let obs = Interface::from_iter([pc1, x1, pc2, x2]);
-    let m = Module::sequential(obs, init, update).unwrap();
+    Module::sequential(obs, init, update)
+}
+
+fn example_tiny1(
+    external: (usize, usize),
+    interface: (usize, usize),
+    wait: bool,
+    base: usize,
+) -> Result<Module<&'static str, &'static str>, &'static str> {
+    let external = [Wire::new(external.0, "Tny"), Wire::new(external.1, "Tny")];
+    let interface = [Wire::new(interface.0, "Tny"), Wire::new(interface.1, "Tny")];
+    let private = [Wire::new(base, "Tny"), Wire::new(base + 1, "Tny")];
+    let temp = Wire::new(base + 2, "Tny");
+
+    let cons = Term::constant("CONST", [temp.clone()]).unwrap();
+
+    let update = if wait {
+        Term::function(
+            "AWAIT",
+            [interface[1].clone(), private[1].clone()],
+            [external[1].clone(), private[0].clone(), temp],
+        )
+        .unwrap()
+    } else {
+        Term::function(
+            "SEQ",
+            [interface[1].clone(), private[1].clone()],
+            [external[0].clone(), private[0].clone(), temp],
+        )
+        .unwrap()
+    };
+
+    let init = Term::constant("INIT", [interface[1].clone(), private[1].clone()]).unwrap();
+
+    let obs = Interface::from_iter([external, interface]);
+    let prvt = Interface::from_iter([private]);
+
+    Module::partially_observable_sequential(obs, prvt, [init], [cons, update])
+}
+
+#[test]
+fn can_instantiate_sequential_module() {
+    let _module = example_counter().unwrap();
+    //print!("{}", _module);
+}
+
+#[test]
+fn can_instantiate_partially_observable_module() {
+    let m = example_counter().unwrap();
+    let wires = m.obs().clone();
+    let mut obs: Vec<[Wire<&'static str>; 2]> = Vec::new();
+    let mut prvt: Vec<[Wire<&'static str>; 2]> = Vec::new();
+    for [ltc, nxt] in wires {
+        if ltc.id() == 0 {
+            prvt.push([ltc, nxt]);
+        } else {
+            obs.push([ltc, nxt]);
+        }
+    }
+    let obs = Interface::from_iter(obs);
+    let prvt = Interface::from_iter(prvt);
+
+    let m = Module::partially_observable(obs, prvt, m.atoms().iter().cloned());
+    //print!("{}", m);
+    assert!(m.is_ok());
+}
+
+#[test]
+fn cannot_instantiate_external_unobservable_wire() {
+    let m = example_counter().unwrap();
+    let wires = m.obs().clone();
+    let mut obs: Vec<[Wire<&'static str>; 2]> = Vec::new();
+    let mut prvt: Vec<[Wire<&'static str>; 2]> = Vec::new();
+    for [ltc, nxt] in wires {
+        if ltc.id() == 3 {
+            prvt.push([ltc, nxt]);
+        } else {
+            obs.push([ltc, nxt]);
+        }
+    }
+    let obs = Interface::from_iter(obs);
+    let prvt = Interface::from_iter(prvt);
+
+    let m = Module::partially_observable(obs, prvt, m.atoms().iter().cloned());
+    //print!("{}", m);
+
+    assert!(m.is_err());
+}
+
+#[test]
+fn can_instantiate_example_peterson1() {
+    let m = example_peterson1().unwrap();
 
     assert!(m.is_open());
     assert_eq!(m.extl()[0].len(), 2);
@@ -218,4 +259,60 @@ fn module_write_all_ctrl() {
 
     let m = Module::sequential(obs.clone(), init, update);
     assert!(m.is_ok());
+}
+
+#[test]
+fn can_compose_example_peterson1_with_empty_module() {
+    let m1 = example_peterson1().unwrap();
+    let m2 = Module::empty();
+
+    let _m3 = Module::parallel([m1, m2]).unwrap();
+}
+
+#[test]
+fn can_instantiate_example_tiny1_right() {
+    let m = example_tiny1((0, 1), (2, 3), true, 4).unwrap();
+    assert!(m.is_open());
+}
+
+#[test]
+fn can_instantiate_example_tiny2_right() {
+    let m = example_tiny1((2, 3), (0, 1), true, 4).unwrap();
+    assert!(m.is_open());
+}
+#[test]
+fn cannot_compose_example_tiny1_with_overlapping_prvt() {
+    let m1 = example_tiny1((0, 1), (2, 3), false, 4).unwrap();
+    let m2 = example_tiny1((2, 3), (0, 1), false, 4).unwrap();
+
+    let m3 = Module::parallel([m1, m2]);
+    assert!(m3.is_err());
+}
+
+#[test]
+fn cannot_compose_example_tiny1_with_cyclic_await() {
+    let m1 = example_tiny1((0, 1), (2, 3), true, 4).unwrap();
+    let m2 = example_tiny1((2, 3), (0, 1), true, 7).unwrap();
+
+    let m3 = Module::parallel([m1, m2]);
+    assert!(m3.is_err());
+}
+
+#[test]
+fn can_compose_example_tiny1_without_cyclic_await_and_overllaping_prvt() {
+    let m1 = example_tiny1((0, 1), (2, 3), true, 4).unwrap();
+    let m2 = example_tiny1((2, 3), (0, 1), false, 7).unwrap();
+
+    let m3 = Module::parallel([m1, m2]);
+    assert!(m3.is_ok());
+}
+
+#[test]
+fn can_compose_three_tiny1_without_cyclic_await_and_overllaping_prvt() {
+    let m1 = example_tiny1((0, 1), (2, 3), true, 4).unwrap();
+    let m2 = example_tiny1((2, 3), (0, 1), false, 7).unwrap();
+    let m3 = example_tiny1((2, 3), (10, 11), false, 12).unwrap();
+
+    let m4 = Module::parallel([m1, m2, m3]);
+    assert!(m4.is_ok());
 }
