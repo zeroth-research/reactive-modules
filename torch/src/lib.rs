db57cbc2b1e1676ba123bb60703bf16bf1653723
Marek Chalupa <marek.chalupa@zeroth.org>
2025-12-01 10:42:18 +0100

    torch: rename ops and types to IType and DType
    To be consistent with other crates.

--- torch/src/lib.rs
+++ torch/src/lib.rs
@@ -6,8 +6,8 @@ mod html;
 
 pub use context::Context;
 
-pub use crate::term::TorchDType as DType;
-pub use crate::term::TorchOp as IType;
+pub use crate::term::DType;
+pub use crate::term::IType;
 pub use crate::term::TorchTerm;
 pub type TorchModule = base::module::Module<DType, IType>;
 pub type TorchAtom = base::atom::Atom<DType, IType>;
