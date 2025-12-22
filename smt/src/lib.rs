14dd92690c3a1c324693ab13d16bb197af221269
Marek Chalupa <marek.chalupa@zeroth.org>
2025-12-22 15:41:01 +0100

    smt: move `src/create_smt_module.rs` to `tests/`

--- smt/src/lib.rs
+++ smt/src/lib.rs
@@ -1,10 +1,4 @@
-// TODO: remove in the future
-#![allow(dead_code)]
-
 pub mod dtype;
 pub mod html;
 pub mod itype;
 pub mod smt;
-
-// Temporary file for creating test SMT modules
-pub mod create_smt_module;
