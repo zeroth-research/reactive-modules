9133eff74fa5fbedf15fb0ea440201ef1e2f8db8
Marek Chalupa <marek.chalupa@zeroth.org>
2026-01-07 17:04:40 +0100

    python/torch: split off translation from context
    It deserves its own space, it is enough code and has little common
    with context itself.

--- python/zrth/torch/context.py
+++ python/zrth/torch/context.py
@@ -2,15 +2,14 @@ from torch import Tensor
 from random import randrange
 from zrth import _zrth
 from zrth.context import Context as ContextBase
-from zrth.expr import Expr, Var, Transform as ExprTransform
+from zrth.expr import Expr, Var
 
 from typing import Callable, Any
 
-from itertools import chain
 
-PyVal = _zrth.PyVal
 WrappedModule = _zrth.torch.WrappedModule
-WrappedTerm = _zrth.torch.WrappedTerm
+
+from .translation import to_terms
 
 
 def handle_return_value(r):
@@ -101,11 +100,11 @@ def choose(*args):
         return _choose_or(alist)
 
 
-class Context_(ContextBase):
+class Context(ContextBase):
     __next_fresh_var = 0
 
-    def __init__(self, ctx_impl):
-        super().__init__(ctx_impl)
+    def __init__(self):
+        super().__init__(_zrth.torch.WrappedContext())
         # map variables to terms that define them
         self.var_to_term: dict[Var, Term] = {}
 
@@ -113,8 +112,8 @@ class Context_(ContextBase):
         return Var(name)
 
     def fresh_var(self) -> Var:
-        Context_.__next_fresh_var += 1
-        return Var(f"__x_{Context_.__next_fresh_var}")
+        Context.__next_fresh_var += 1
+        return Var(f"__x_{Context.__next_fresh_var}")
 
     def constant(self, *args):
         return Tensor(*args)
@@ -178,11 +177,6 @@ class Context_(ContextBase):
 
         return r
 
-
-class Context(Context_):
-    def __init__(self):
-        super().__init__(_zrth.torch.WrappedContext())
-
     def module_from_methods(
         self,
         ctrl: str | tuple[str],
@@ -216,168 +210,11 @@ class Context(Context_):
         assert len(update_ret) == len(ctrl)
 
         # translate all that we have into terms
-        cur_vars, nxt_vars, init_terms, update_terms = self.to_terms(
-            ctrl, extl, init_ret, update_ret
+        cur_vars, nxt_vars, init_terms, update_terms = to_terms(
+            self, ctrl, extl, init_ret, update_ret
         )
 
         # create the Rust module
         return WrappedModule(
             self._context, cur_vars, nxt_vars, init_terms, update_terms
         )
-
-    def var_to_pyval(self, var: Var) -> PyVal:
-        return self.unwrap().var(var.name)
-
-    def to_terms(self, ctrl, extl, init_ret, update_ret) -> (list, list, list, list):
-        """
-        MODIFIES `self._context`
-        """
-        walker = Translate(self)
-
-        init_terms = []
-        assert len(init_ret) == len(ctrl)
-        for var, expr in zip(ctrl, init_ret):
-            tmp, outvar = walker.translate(expr)
-            init_terms.extend(tmp)
-
-            # map the output of the expression to the output wire
-            assert len(outvar) == 1, outvar
-            init_terms.append(
-                WrappedTerm("Id", outvar, [self.var_to_pyval(self.next_var(var))])
-            )
-
-        update_terms = []
-        assert len(update_ret) == len(ctrl)
-        for var, expr in zip(ctrl, update_ret):
-            tmp, outvar = walker.translate(expr)
-            update_terms.extend(tmp)
-
-            # map the output of the expression to the output wire
-            assert len(outvar) == 1
-            update_terms.append(
-                WrappedTerm("Id", outvar, [self.var_to_pyval(self.next_var(var))])
-            )
-
-        cur_vars = [self.var_to_pyval(v) for v in chain(ctrl, extl)]
-        nxt_vars = [self.var_to_pyval(self.next_var(v)) for v in chain(ctrl, extl)]
-        return cur_vars, nxt_vars, init_terms, update_terms
-
-
-def rel_to_str(op: str) -> str:
-    if op == "lt":
-        return "Lt"
-    if op == "gt":
-        return "Gt"
-    if op == "le":
-        return "Le"
-    if op == "ge":
-        return "Ge"
-    if op == "eq":
-        return "Eq"
-
-    raise NotImplementedError(f"Unknown relation: {opty}")
-
-
-def arith_to_str(op) -> str:
-    if op == "add":
-        return "Add"
-    if op == "mul":
-        return "Mul"
-    if op == "matmul":
-        return "MatMul"
-
-    raise NotImplementedError(f"Unknown operation: {op}")
-
-
-class Translate(ExprTransform):
-    def __init__(self, ctx) -> None:
-        """
-        MODIFIES `ctx`
-        """
-        self.ctx = ctx
-        # FIXME: this is confusing
-        self._ctx = ctx.unwrap()
-        self.terms = []
-
-    def translate(self, formula):
-        """
-        Translate a formula into reactive module terms.
-        Return the terms and the output variable (wire) for the formula.
-        """
-        self.terms = []
-        r = self.transform(formula)
-        return self.terms, r
-
-    def default(self, expr, args):
-        raise NotImplementedError(f"Not implemented translation of operation: `{expr}`")
-
-    # def before_all(self, expr: Expr, args: list):
-    #    print("VIS", expr, type(expr))
-
-    # def forall(self, expr: Expr, args: list):
-    #    assert all(isinstance(a, PyVal) for a in args), args
-
-    def visit_type_bool(self, expr):
-        return [PyVal.bool(expr)]
-
-    def visit_type_int(self, expr):
-        return [PyVal.int(expr)]
-
-    def visit_type(self, expr, args):
-        if isinstance(expr, Tensor):
-            return [PyVal.tensor(expr)]
-        return self.default(expr, args)
-
-    def visit_var(self, expr, args):
-        return [self._ctx.var(expr.name)]
-
-    def visit_arith(self, expr, args, op):
-        assert len(expr.args) == 2
-        assert args[0].dtype() == args[1].dtype(), args
-        out = self._ctx.tmp_var()
-        self.terms.append(WrappedTerm(arith_to_str(op), reads=args, writes=[out]))
-        return [out]
-
-    def visit_cmp(self, expr, args, op):
-        assert len(args) == 2
-        out = self._ctx.tmp_var()
-        self.terms.append(WrappedTerm(rel_to_str(op), reads=args, writes=[out]))
-        return [out]
-
-    def visit_logic(self, expr, args, op):
-        if op == "not":
-            assert len(args) == 1
-            out = self._ctx.tmp_var()
-            self.terms.append(WrappedTerm("Not", reads=args, writes=[out]))
-        else:
-            assert len(args) == 2
-            assert op in ("and", "or"), op
-            out = self._ctx.tmp_var()
-            op = "Or" if op == "or" else "And"
-            self.terms.append(WrappedTerm(op, reads=args, writes=[out]))
-        return [out]
-
-    def visit_ite(self, expr, args):
-        assert len(args) == 3
-        assert args[1].dtype() == args[2].dtype(), args
-        out = self._ctx.tmp_var()
-        self.terms.append(WrappedTerm("Cond", reads=args, writes=[out]))
-        return [out]
-
-    def visit_ifthen(self, expr, args):
-        assert len(args) == 2
-        out = self._ctx.tmp_var()
-        self.terms.append(WrappedTerm("IfThen", reads=args, writes=[out]))
-        return [out]
-
-    def visit_choose(self, expr, args):
-        assert len(args) > 0, args
-        out = self._ctx.tmp_var()
-        self.terms.append(WrappedTerm("Choose", reads=args, writes=[out]))
-        return [out]
-
-    def visit_choose_or(self, expr, args):
-        assert len(args) > 0, args
-        out = self._ctx.tmp_var()
-        self.terms.append(WrappedTerm("ChooseOr", reads=args, writes=[out]))
-        return [out]
