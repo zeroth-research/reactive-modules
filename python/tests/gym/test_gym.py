025d7acaf5b0f9ed206755377e6bf00a2e6a0f8e
Marek Chalupa <marek.chalupa@zeroth.org>
2026-01-20 20:28:49 +0100

    python/tests: make `gym/test.py` compatible with PyTest

--- python/tests/gym/test_gym.py
+++ python/tests/gym/test_gym.py
@@ -1,39 +1,43 @@
 from zrth.gym import Context, QNetwork, SimpleEnv
-from zrth import Module
+# from zrth import Module
 
 
-def test_qnetwork_conversion():    
+def qnetwork():
     ctx = Context()
     qnet = QNetwork(
-        extl = ['observation'],
-        intf = ['q_values'],
+        extl=["observation"],
+        intf=["q_values"],
         state_size=1,
         action_size=2,
         hidden_size=2,
-        ctx=ctx
+        ctx=ctx,
     )
-    
+
     print(f"QNetwork reactive module:\n{qnet._reactive_module}")
     print(ctx)
-    print(f"\nWires:")
+    print("\nWires:")
     print(f"  qnet.intf = {qnet.intf}")
     print(f"  qnet.extl = {qnet.extl}")
 
     return qnet
 
 
-def test_simpleenv_conversion():
+def test_qnetwork_conversion():
+    _ = qnetwork()
+
+
+def simpleenv():
     ctx = Context()
     env = SimpleEnv(
-        extl = ['q_values'],
-        intf = ['observation', 'reward', 'terminated'],
-        prvt = ['state'],
-        ctx=ctx
+        extl=["q_values"],
+        intf=["observation", "reward", "terminated"],
+        prvt=["state"],
+        ctx=ctx,
     )
-    
+
     print(f"\nSimpleEnv reactive module: {env._reactive_module}")
     print(ctx)
-    print(f"\nWire access:")
+    print("\nWire access:")
     print(f"  env.intf = {env.intf}")
     print(f"  env.extl = {env.extl}")
     print(f"  env.prvt = {env.prvt}")
@@ -41,8 +45,13 @@ def test_simpleenv_conversion():
     return env
 
 
-if __name__ == '__main__':
-    qnet = test_qnetwork_conversion()
+def test_simpleenv_conversion():
+    _ = simpleenv()
+
+
+if __name__ == "__main__":
+    qnet = qnetwork()
     # print("\n" + "="*60 + "\n")
     # env = test_simpleenv_conversion()
     # composed = Module.parallel(qnet._reactive_module, env._reactive_module)
+
