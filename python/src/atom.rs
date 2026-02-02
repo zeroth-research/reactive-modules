e805eb434aaa47ed2e417f9729124b1c647b4d3b
Marek Chalupa <marek.chalupa@zeroth.org>
2026-02-02 09:36:40 +0100

    python: allow iterating over terms in atoms

--- python/src/atom.rs
+++ python/src/atom.rs
@@ -1,3 +1,4 @@
+use crate::term::TermInterfaceType;
 use crate::wire::Wire;
 use crate::{DType, IType, try_iter_borrow};
 use pyo3::exceptions::PyIndexError;
@@ -28,6 +29,20 @@ impl Atom {
         Self::interface(slf, AtomInterfaceType::Await)
     }
 
+    fn init(slf: Bound<'_, Self>) -> AtomTerms {
+        AtomTerms {
+            atom: slf.unbind(),
+            typ: AtomTermType::Init,
+        }
+    }
+
+    fn update(slf: Bound<'_, Self>) -> AtomTerms {
+        AtomTerms {
+            atom: slf.unbind(),
+            typ: AtomTermType::Update,
+        }
+    }
+
     fn __str__(&self) -> String {
         self.base.to_string()
     }
@@ -51,6 +66,7 @@ enum AtomInterfaceType {
     Ctrl,
     Await,
 }
+
 #[pyclass(sequence)]
 struct AtomInterface {
     atom: Py<Atom>,
@@ -101,6 +117,157 @@ impl AtomInterface {
                 (None, None) => return true,
                 _ => return false,
             }
+        }
+    }
+}
+
+#[derive(Clone)]
+enum AtomTermType {
+    Init,
+    Update,
+}
+
+/// An accessor for a Term in an [Atom]
+#[pyclass(frozen)]
+struct AtomTerm {
+    atom: Py<Atom>,
+    typ: AtomTermType,
+    idx: usize,
+}
+
+impl AtomTerm {
+    fn get(&self) -> &base::Term<DType, IType> {
+        let atom = &self.atom.get().base;
+        match self.typ {
+            AtomTermType::Init => atom.init().get(self.idx).unwrap(),
+            AtomTermType::Update => atom.update().get(self.idx).unwrap(),
+        }
+    }
+}
+
+#[pymethods]
+impl AtomTerm {
+    fn __str__(&self) -> String {
+        self.get().to_string()
+    }
+
+    fn itype(&self) -> IType {
+        self.get().itype().clone()
+    }
+
+    fn read(slf: Bound<'_, Self>) -> AtomTermInterface {
+        AtomTermInterface {
+            term: slf.unbind(),
+            interface: TermInterfaceType::Read,
+        }
+    }
+
+    fn write(slf: Bound<'_, Self>) -> AtomTermInterface {
+        AtomTermInterface {
+            term: slf.unbind(),
+            interface: TermInterfaceType::Write,
+        }
+    }
+}
+
+#[pyclass(sequence)]
+struct AtomTermInterface {
+    term: Py<AtomTerm>,
+    interface: TermInterfaceType,
+}
+
+impl AtomTermInterface {
+    fn base(&self) -> &base::Interface<DType> {
+        let term = self.term.get().get();
+        match self.interface {
+            TermInterfaceType::Read => term.read(),
+            TermInterfaceType::Write => term.write(),
+        }
+    }
+}
+
+#[pymethods]
+impl AtomTermInterface {
+    fn __str__(&self) -> String {
+        self.base().to_string()
+    }
+
+    fn __eq__(&self, other: &Bound<'_, PyAny>) -> bool {
+        let other = match try_iter_borrow::<Wire>(other) {
+            Ok(other) => other,
+            Err(_) => return false,
+        };
+
+        let mut this = self.base().wires();
+        let mut other = other.into_iter();
+        loop {
+            match (this.next(), other.next()) {
+                (Some(this), Some(Ok(other))) => {
+                    if this != other.base() {
+                        return false;
+                    }
+                }
+                (None, None) => return true,
+                _ => return false,
+            }
+        }
+    }
+
+    fn __getitem__(&self, index: usize) -> PyResult<Wire> {
+        let item = self.base().wire(0, index);
+        item.and_then(|w| Some(w.clone().into()))
+            .ok_or(PyIndexError::new_err("index out of bounds"))
+    }
+
+    fn __len__(&self) -> usize {
+        self.base().len()
+    }
+}
+
+#[pyclass(sequence)]
+struct AtomTerms {
+    atom: Py<Atom>,
+    typ: AtomTermType,
+}
+
+impl AtomTerms {
+    fn atom(&self) -> &base::Atom<DType, IType> {
+        &self.atom.get().base
+    }
+}
+
+#[pymethods]
+impl AtomTerms {
+    fn __getitem__(slf: PyRef<'_, Self>, index: usize) -> PyResult<AtomTerm> {
+        let atom = slf.atom();
+        match slf.typ {
+            AtomTermType::Init => {
+                if index < atom.init().len() {
+                    return Ok(AtomTerm {
+                        atom: slf.atom.clone_ref(slf.py()),
+                        typ: slf.typ.clone(),
+                        idx: index,
+                    });
+                }
+                Err(PyIndexError::new_err("index out of bounds"))
+            }
+            AtomTermType::Update => {
+                if index < atom.update().len() {
+                    return Ok(AtomTerm {
+                        atom: slf.atom.clone_ref(slf.py()),
+                        typ: slf.typ.clone(),
+                        idx: index,
+                    });
+                }
+                Err(PyIndexError::new_err("index out of bounds"))
+            }
+        }
+    }
+
+    fn __len__(&self) -> usize {
+        match self.typ {
+            AtomTermType::Init => self.atom().init().len(),
+            AtomTermType::Update => self.atom().update().len(),
         }
     }
 }
