b4b3e5627f434b609bd6a884a43c30169bb1cfe9
Marek Chalupa <marek.chalupa@zeroth.org>
2026-02-12 10:21:59 +0100

    Improve typing

--- python/zrth/bmc.py
+++ python/zrth/bmc.py
@@ -1,14 +1,13 @@
-from zrth import Transition, WiredTransitions
-from zrth.context import get_ctx
+from zrth import WiredTransitions
 
 
 class ModuleUnrolling:
     def __init__(self, m) -> None:
         self._module = m
         # initialized by `init`
-        self._transitions = None
+        self._transitions: None | WiredTransitions = None
 
-    def transitions(self) -> WiredTransitions:
+    def transitions(self) -> WiredTransitions | None:
         return self._transitions
 
     def init(self):
@@ -20,6 +19,7 @@ class ModuleUnrolling:
         self._transitions.step(self._module.unwrap(), self._module._ctx.unwrap())
 
     def to_html(self, path: str, open: bool = False):
+        assert self._transitions
         self._transitions.to_html(self._module._ctx.unwrap(), path)
 
         if open:
@@ -33,7 +33,9 @@ class ModuleUnrolling:
             # FIXME: not exhaustive
 
     def dbg(self):
+        assert self._transitions
         self._transitions.dbg()
 
     def last_state(self):
+        assert self._transitions
         self._transitions.last_state()
