7661a414acc624069b2be5b5ce30d8bc64d55e67
Mirco Giacobbe <mirco.giacobbe@gmail.com>
2026-02-15 14:40:43 +0000

    Added global wire counter to pyo3 wrapper
    @mchalupa: rebased

--- python/tests/test_base.py
+++ python/tests/test_base.py
@@ -3,61 +3,62 @@ from torch import Tensor
 
 
 def test_wire_new():
-    x = Wire(0, dt.Bool)
-    y = Wire(1, dt.Bool)
+    Wire(dt.Bool)
+    Wire(dt.Int)
+    Wire(dt.TensorInt([1, 2, 3]))
 
 
 def test_term_new():
-    x = Wire(1, dt.TensorInt([2, 2, 3]))
-    y = Wire(2, dt.TensorInt([2, 2, 3]))
-    xn = Wire(3, dt.TensorInt([2, 2, 3]))
-    w4 = Wire(4, dt.TensorFloat([3]))
-    w5 = Wire(5, dt.TensorInt([3]))
+    x = Wire(dt.TensorInt([2, 2, 3]))
+    y = Wire(dt.TensorInt([2, 2, 3]))
+    xn = Wire(dt.TensorInt([2, 2, 3]))
+    w4 = Wire(dt.TensorFloat([3]))
+    w5 = Wire(dt.TensorInt([3]))
 
     # test `function` ctor
-    f = Term.function(it.Add(), [xn], [x, y])
-    g = Term.function(it.Tensor(Tensor([3, 4, 5])), [w4], [])
-    h = Term.function(it.Tensor(Tensor([3, 4, 6])), [w5], [])
+    _ = Term.function(it.Add(), [xn], [x, y])
+    _ = Term.function(it.Tensor(Tensor([3, 4, 5])), [w4], [])
+    _ = Term.function(it.Tensor(Tensor([3, 4, 6])), [w5], [])
 
     # test `new` ctor
-    Term(it.Lt(), [Wire(6, dt.Bool)], [w4, w5])
-    Term(it.Tensor(Tensor([3, 2, 1])), [Wire(7, dt.TensorInt([3]))])
+    Term(it.Lt(), [Wire(dt.Bool)], [w4, w5])
+    Term(it.Tensor(Tensor([3, 2, 1])), [Wire(dt.TensorInt([3]))])
 
 
 def test_mk_term():
-    x = Wire(1, dt.TensorInt([2, 2, 3]))
-    y = Wire(2, dt.TensorInt([2, 2, 3]))
-    xn = Wire(3, dt.TensorInt([2, 2, 3]))
+    x = Wire(dt.TensorInt([2, 2, 3]))
+    y = Wire(dt.TensorInt([2, 2, 3]))
+    xn = Wire(dt.TensorInt([2, 2, 3]))
 
-    f = mk_term(it.Add(), [xn], [x, y])
-    g = mk_term(it.Tensor(Tensor([3, 4, 5])), [Wire(4, dt.TensorInt([3]))], [])
-    h = mk_term(it.Tensor(Tensor([3, 4, 6])), [Wire(5, dt.TensorInt([3]))], [])
+    _ = mk_term(it.Add(), [xn], [x, y])
+    g = mk_term(it.Tensor(Tensor([3, 4, 5])), [Wire(dt.TensorInt([3]))], [])
+    h = mk_term(it.Tensor(Tensor([3, 4, 6])), [Wire(dt.TensorInt([3]))], [])
 
     # test `new` ctor
-    t1 = mk_term(it.Lt(), [Wire(6, dt.Bool)], [g, h])
-    t2 = mk_term(it.Tensor(Tensor([3, 2, 1])), [Wire(7, dt.TensorInt([3]))])
+    _ = mk_term(it.Lt(), [Wire(dt.Bool)], [g, h])
+    _ = mk_term(it.Tensor(Tensor([3, 2, 1])), [Wire(dt.TensorInt([3]))])
 
 
 def test_module_sequential():
-    x = (Wire(0, dt.Bool), Wire(1, dt.Bool))
+    x = (Wire(dt.Bool), Wire(dt.Bool))
     init = [Term(it.Tensor(Tensor([True])), [x[1]])]
     update = [Term(it.Id(), [x[1]], [x[0]])]
-    m = Module.sequential(init, update, [x])
+    _ = Module.sequential(init, update, [x])
 
 
 def test_module_combinatorial():
-    x = (Wire(0, dt.Bool), Wire(1, dt.Bool))
+    x = (Wire(dt.Bool), Wire(dt.Bool))
 
     assign = [Term(it.Tensor(Tensor([False])), [x[1]])]
-    m = Module.combinatorial(assign, [x])
+    _ = Module.combinatorial(assign, [x])
 
 
 def test_module_parallel():
-    x = (Wire(0, dt.Bool), Wire(1, dt.Bool))
-    y = (Wire(2, dt.Bool), Wire(3, dt.Bool))
-    z = (Wire(4, dt.Bool), Wire(5, dt.Bool))
-    w = (Wire(6, dt.Bool), Wire(7, dt.Bool))
-    v = (Wire(8, dt.Bool), Wire(9, dt.Bool))
+    x = (Wire(dt.Bool), Wire(dt.Bool))
+    y = (Wire(dt.Bool), Wire(dt.Bool))
+    z = (Wire(dt.Bool), Wire(dt.Bool))
+    w = (Wire(dt.Bool), Wire(dt.Bool))
+    v = (Wire(dt.Bool), Wire(dt.Bool))
 
     init = [Term(it.Tensor(Tensor([False])), [x[1]])]
     update = [Term(it.And(), [x[1]], [x[0], y[1]])]
@@ -91,9 +92,9 @@ def test_module_parallel():
 
 
 def test_interface():
-    x = Wire(0, dt.Bool)
-    y = Wire(1, dt.Bool)
-    xn = Wire(2, dt.Bool)
+    x = Wire(dt.Bool)
+    y = Wire(dt.Bool)
+    xn = Wire(dt.Bool)
     f = Term(it.Id(), [xn], [x, y])
     f2 = Term(it.Id(), [xn], [x, y])
 
