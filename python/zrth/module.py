6a290f215e353f84377e842147a82e003ca5f60d
Marek Chalupa <marek.chalupa@zeroth.org>
2026-02-20 23:54:12 +0100

    python: fix gym after rebasing

--- python/zrth/module.py
+++ python/zrth/module.py
@@ -1,8 +1,20 @@
-from .eexpr import nxt, Expr, Sym, sym
-from .context import Context, get_ctx
-from .zrth import DType, Module as RustModule, Transition
+# from .context import Context, get_ctx
+from .zrth import DType, Module as RustModule, Transition, Wire
 
 from typing import Callable
+
+
+class WirePair(tuple):
+    def __new__(cls, *elms):
+        assert all(isinstance(e, Wire) for e in elms)
+        assert len(elms) == 2
+        return super().__new__(cls, elms)
+
+    def latched(self) -> Wire:
+        return self[0]
+
+    def nxt(self) -> Wire:
+        return self[1]
 
 
 class ReactiveModuleDef:
@@ -63,16 +75,16 @@ class ReactiveModuleDef:
 
     def __init__(
         self,
-        intf: str | tuple[str] | Sym | tuple[Sym] | None = None,
-        extl: str | tuple[str] | Sym | tuple[Sym] | None = None,
-        prvt: str | tuple[str] | Sym | tuple[Sym] | None = None,
-        rust_module: RustModule = None,
-        name: str = None,
+        intf: str | tuple[str, ...] | None = None,
+        extl: str | tuple[str, ...] | None = None,
+        prvt: str | tuple[str, ...] | None = None,
+        rust_module: None | RustModule = None,
+        name: None | str = None,
     ):
-        self._ctx = get_ctx()
-        self._intf_syms = parse_variables(intf)
-        self._extl_syms = parse_variables(extl)
-        self._prvt_syms = parse_variables(prvt)
+        # self._ctx = get_ctx()
+        self._intf_wires, self._intf_names = parse_variables(intf)
+        self._extl_wires, self._extl_names = parse_variables(extl)
+        self._prvt_wires, self._prvt_names = parse_variables(prvt)
 
         # Not used ATM
         self._name = name
@@ -81,26 +93,72 @@ class ReactiveModuleDef:
         self._module = rust_module
 
     @property
-    def intf(self):
-        return self._intf_syms
+    def intf(self) -> tuple[Wire, ...]:
+        return tuple(w[0] for w in self._intf_wires)
 
     @property
-    def extl(self):
-        return self._extl_syms
+    def intf2(self) -> tuple[WirePair, ...]:
+        return self._intf_wires
 
     @property
-    def prvt(self):
-        return self._prvt_syms
+    def extl(self) -> tuple[Wire, ...]:
+        return tuple(w[0] for w in self._extl_wires)
 
     @property
-    def obs(self):
+    def extl2(self) -> tuple[WirePair, ...]:
+        return self._extl_wires
+
+    @property
+    def prvt(self) -> tuple[Wire, ...]:
+        return tuple(w[0] for w in self._prvt_wires)
+
+    @property
+    def prvt2(self) -> tuple[WirePair, ...]:
+        return self._prvt_wires
+
+    @property
+    def obs(self) -> tuple[Wire, ...]:
         """Observable wires: external inputs + interface outputs"""
         return self.extl + self.intf
 
     @property
-    def ctrl(self):
+    def obs2(self) -> tuple[WirePair, ...]:
+        """Observable wires: external inputs + interface outputs"""
+        return self.extl2 + self.intf2
+
+    @property
+    def ctrl(self) -> tuple[Wire, ...]:
         """Controlled wires: interface outputs + private wires"""
-        return self.intf + (self.prvt or [])
+        return self.intf + (self.prvt or ())
+
+    @property
+    def ctrl2(self) -> tuple[WirePair, ...]:
+        """Controlled wires: interface outputs + private wires"""
+        return self.intf2 + (self.prvt2 or ())
+
+    @property
+    def intf_names(self) -> tuple[str, ...]:
+        return self._intf_names
+
+    @property
+    def extl_names(self) -> tuple[str, ...]:
+        return self._extl_names
+
+    @property
+    def prvt_names(self) -> tuple[str, ...]:
+        return self._prvt_names
+
+    def name_to_wires(self) -> dict[str, WirePair]:
+        return {
+            name: wp
+            for name, wp in zip(
+                self._extl_names + self._intf_names + self._prvt_names,
+                self._extl_wires + self._intf_wires + self._prvt_wires,
+            )
+        }
+
+    def convert(self):
+        raise RuntimeWarning("Convert not defined")
 
     @property
     def atoms(self):
@@ -112,15 +170,15 @@ class ReactiveModuleDef:
     def update_as_transition(self) -> Transition:
         return self._module.update_as_transition()
 
-    def unwrap(self) -> RustModule:
+    def unwrap(self) -> RustModule | None:
         return self._module
 
     def __repr__(self) -> str:
         return f"""
-{__class__} {{
-  intf: {tuple(str(v) for v in self.intf)}
-  extl: {tuple(str(v) for v in self.extl)}
-  prvt: {tuple(str(v) for v in self.prvt)}
+{self.__class__} {{
+  intf: {", ".join(f"{n}: {v}" for n, v in zip(self._intf_names, self._intf_wires))}
+  extl: {", ".join(f"{n}: {v}" for n, v in zip(self._extl_names, self._extl_wires))}
+  prvt: {", ".join(f"{n}: {v}" for n, v in zip(self._prvt_names, self._prvt_wires))}
   module:
   {self._module.__repr__() if self._module else "<no rust module>"}
 }}
@@ -129,26 +187,27 @@ class ReactiveModuleDef:
     def __str__(self) -> str:
         return f"""
 ReactiveModule {{
-  intf: {", ".join(v.name for v in self.intf)}
-  extl: {", ".join(v.name for v in self.extl)}
-  prvt: {", ".join(v.name for v in self.prvt)}
-  -------
-  {self._module.__str__() if self._module else "<no rust module>"}
+  intf: {", ".join(f"{n}: {v}" for n, v in zip(self._intf_names, self._intf_wires))}
+  extl: {", ".join(f"{n}: {v}" for n, v in zip(self._extl_names, self._extl_wires))}
+  prvt: {", ".join(f"{n}: {v}" for n, v in zip(self._prvt_names, self._prvt_wires))}
+  module:
+  {self._module.__repr__() if self._module else "<no rust module>"}
 }}
 """
 
-    def to_html(self, path: str, open: bool = False):
-        self._module.to_html(self._ctx.unwrap(), path)
 
-        if open:
-            from sys import platform
-            from subprocess import run
-
-            if platform == "linux":
-                _ = run(["xdg-open", path])
-            else:
-                _ = run(["open", path])
-            # FIXME: not exhaustive
+# def to_html(self, path: str, open: bool = False):
+#    self._module.to_html(self._ctx.unwrap(), path)
+#
+#    if open:
+#        from sys import platform
+#        from subprocess import run
+#
+#        if platform == "linux":
+#            _ = run(["xdg-open", path])
+#        else:
+#            _ = run(["open", path])
+#        # FIXME: not exhaustive
 
 
 class ReactiveModule(ReactiveModuleDef):
@@ -174,29 +233,29 @@ class ReactiveModule(ReactiveModuleDef):
 
     def __init__(
         self,
-        intf: str | tuple[str] | Sym | tuple[Sym] | None,
-        extl: str | tuple[str] | Sym | tuple[Sym] | None = None,
-        prvt: str | tuple[str] | Sym | tuple[Sym] | None = None,
-        name: str = None,
+        intf: str | tuple[str, ...] | None,
+        extl: str | tuple[str, ...] | None = None,
+        prvt: str | tuple[str, ...] | None = None,
+        name: None | str = None,
     ):
         super().__init__(intf, extl, prvt, name=name)
 
         # convert will be called automatically after sub-class's __init__ finishes
 
-    def convert(self):
-        assert hasattr(self, "update"), type(self)
-        assert self._module is None
-
-        if self.prvt:
-            raise NotImplementedError("Private variables not implemented yet")
-
-        if not hasattr(self, "init"):
-            self.init = None
-
-        self._module = rust_module_from_methods(
-            self.ctrl, self.extl, self.init, self.update, name=self._name
-        )
-
+    # def convert(self):
+    #     assert hasattr(self, "update"), type(self)
+    #     assert self._module is None
+    #
+    #     if self.prvt:
+    #         raise NotImplementedError("Private variables not implemented yet")
+    #
+    #     if not hasattr(self, "init"):
+    #         self.init = None
+    #
+    #     self._module = rust_module_from_methods(
+    #         self.ctrl, self.extl, self.init, self.update, name=self._name
+    #     )
+    #
     def __init_subclass__(cls, **kwargs):
         """
         Hook to automatically call conversion after subclass' __init__.
@@ -226,7 +285,9 @@ def call_convert_after_init(thiscls, cls, **kwargs):
     cls.__init__ = wrapped_init
 
 
-def parse_variables(variables) -> tuple[Sym]:
+def parse_variables(
+    variables,
+) -> tuple[tuple[WirePair, ...], tuple[str, ...]]:
     """
     Parse variables (ctrl or extl) given to the module.
 
@@ -240,10 +301,10 @@ def parse_variables(variables) -> tuple[Sym]:
     if we returned pairs when inputs are strings. We can get next through
     `Sym.nxt` method.
 
-    :return: a list of `Sym` objects.
+    :return: a list of `(Wire, Wire)` and a list of names
     """
     if not variables:
-        return ()
+        return (), ()
 
     if isinstance(variables, str):
         V = variables.split(",")
@@ -254,103 +315,103 @@ def parse_variables(variables) -> tuple[Sym]:
             f"Expect variables to be a string, tuple or list, got: {variables}"
         )
 
-    syms = []
+    syms: list[WirePair] = []
+    names: list[str] = []
     for v in V:
-        if isinstance(v, str):
-            if not ":" in v:
-                raise RuntimeError(f"A variable `{v}` is missing a type annotation")
-            v = v.split(":")
-            v = sym(v[0].strip(), DType.from_str(v[1].strip()), create_pair=True)[0]
-        elif not isinstance(v, Sym):
-            raise RuntimeError(
-                f"A variable must be given as a string or `Sym` object, got {v} ({type(v)})"
-            )
-            # test if the symbol has been created with next symbol,
-            # this is a requirement
-            v.nxt()
+        if not isinstance(v, str):
+            raise ValueError(f"Invalid variable specification: `{v}`")
 
-        syms.append(v)
+        if ":" not in v:
+            raise RuntimeError(f"A variable `{v}` is missing a type annotation")
 
-    return syms
+        v: list[str] = v.split(":")
+        name, dtype = v[0].strip(), v[1].strip()
+        dtype = DType.from_str(dtype)
+        syms.append(WirePair(Wire(dtype), Wire(dtype)))
+        names.append(name)
+
+    return tuple(syms), tuple(names)
 
 
-def rust_module_from_methods(
-    ctrl: tuple[Sym],
-    extl: tuple[Sym],
-    init: Callable[[], None],
-    update: Callable[[], None],
-    name: str | None = None,
-) -> RustModule:
-    """
-    Create the Rust module from the `init` and `update` methods.
-    """
-    assert all(isinstance(v, Sym) for v in ctrl)
-    assert all(isinstance(v, Sym) for v in extl)
-    # assert all(isinstance(v, Sym) for v in prvt)
-
-    # take only the latched Symbols from the tuples of variables,
-    # next values will be accessed through `nxt` or `[1]`
-    # if the user uses a single variable, it is more natural in the `init` and `update` to unwrap it
-    ctrl_arg = ctrl[0] if len(ctrl) == 1 else tuple(*ctrl)
-    extl_arg = extl[0] if len(extl) == 1 else tuple(*extl)
-
-    # trace the init function
-    if init:
-        # say context to gather terms
-        init_ret, init_terms = trace_fun(ctrl, init, extl_arg)
-        assert len(init_ret) == len(ctrl), (init_ret, ctrl)
-    else:
-        init_terms = []
-
-    # trace the update function
-    update_ret, update_terms = trace_fun(ctrl, update, ctrl_arg, extl_arg)
-    assert len(update_ret) == len(ctrl), (len(update_ret), len(ctrl))
-
-    # create the Rust module
-    return RustModule.sequential(
-        init_terms,
-        update_terms,
-        [(s.wire(), s.nxt().wire()) for s in ctrl]
-        + [(s.wire(), s.nxt().wire()) for s in extl],
-    )
-
-
-def trace_fun(ctrl, fun, *args):
-    ctx = get_ctx()
-
-    # say context to gather terms
-    terms = []
-    ctx.push_terms_frame(terms)
-
-    # get the expressions (and terms) for the function
-    ret = handle_return_value(fun(*args))
-
-    # wire the output of the function with control variables
-    assert len(ret) == len(ctrl)
-    ret = [
-        Expr(
-            "assign",
-            expr,  # what
-            s.nxt(),  # to
-        )
-        for s, expr in zip(ctrl, ret)
-    ]
-
-    ctx.pop_terms_frame()
-    return ret, terms
+#
+# def rust_module_from_methods(
+#     ctrl: tuple[WirePair],
+#     extl: tuple[WirePair],
+#     init: Callable[[], None],
+#     update: Callable[[], None],
+#     name: str | None = None,
+# ) -> RustModule:
+#     """
+#     Create the Rust module from the `init` and `update` methods.
+#     """
+#     assert all(isinstance(wt, WirePair) for wt in ctrl), ctrl
+#     assert all(isinstance(wt, WirePair) for wt in extl), extl
+#     # assert all(isinstance(v, Sym) for v in prvt)
+#
+#     # take only the latched Symbols from the tuples of variables,
+#     # next values will be accessed through `nxt` or `[1]`
+#     # if the user uses a single variable, it is more natural in the `init` and `update` to unwrap it
+#     ctrl_arg = ctrl[0] if len(ctrl) == 1 else tuple(*ctrl)
+#     extl_arg = extl[0] if len(extl) == 1 else tuple(*extl)
+#
+#     # trace the init function
+#     if init:
+#         # say context to gather terms
+#         init_ret, init_terms = trace_fun(ctrl, init, extl_arg)
+#         assert len(init_ret) == len(ctrl), (init_ret, ctrl)
+#     else:
+#         init_terms = []
+#
+#     # trace the update function
+#     update_ret, update_terms = trace_fun(ctrl, update, ctrl_arg, extl_arg)
+#     assert len(update_ret) == len(ctrl), (len(update_ret), len(ctrl))
+#
+#     # create the Rust module
+#     return RustModule.sequential(
+#         init_terms,
+#         update_terms,
+#         [(s.wire(), s.nxt().wire()) for s in ctrl]
+#         + [(s.wire(), s.nxt().wire()) for s in extl],
+#     )
 
 
-def handle_return_value(r):
-    """
-    Transform the return value into a list
-    """
-
-    if r is None:
-        r = []
-    elif isinstance(r, tuple):
-        r = [x for x in r]
-    elif not isinstance(r, list):
-        r = [r]
-
-    # return [x.var if isinstance(x, Assignment) else x for x in r]
-    return r
+#
+# def trace_fun(ctrl, fun, *args):
+#     ctx = get_ctx()
+#
+#     # say context to gather terms
+#     terms = []
+#     ctx.push_terms_frame(terms)
+#
+#     # get the expressions (and terms) for the function
+#     ret = handle_return_value(fun(*args))
+#
+#     # wire the output of the function with control variables
+#     assert len(ret) == len(ctrl)
+#     ret = [
+#         Expr(
+#             "assign",
+#             expr,  # what
+#             s.nxt(),  # to
+#         )
+#         for s, expr in zip(ctrl, ret)
+#     ]
+#
+#     ctx.pop_terms_frame()
+#     return ret, terms
+#
+#
+# def handle_return_value(r):
+#     """
+#     Transform the return value into a list
+#     """
+#
+#     if r is None:
+#         r = []
+#     elif isinstance(r, tuple):
+#         r = [x for x in r]
+#     elif not isinstance(r, list):
+#         r = [r]
+#
+#     # return [x.var if isinstance(x, Assignment) else x for x in r]
+#     return r
