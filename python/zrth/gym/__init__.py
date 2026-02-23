6826424b7bc0b5c6e87c1ebbd895014c05f874ea
Marek Chalupa <marek.chalupa@zeroth.org>
2026-02-23 15:36:28 +0100

    python/gym: remove transitive imports
    Do not import and expose parts not defined in Gym.

--- python/zrth/gym/__init__.py
+++ python/zrth/gym/__init__.py
@@ -1,13 +1,16 @@
 """Reinforcement Learning with Reactive Modules"""
 
-from .zrth_module import ExtendedModule
 from .qnetworks import SimpleQNet, GridWorldQNet
-from .environments import SimpleEnv, GridWorldEnv, ComplexDecisionEnv, EarlyReturnEnv, ComparisonChainEnv
+from .environments import (
+    SimpleEnv,
+    GridWorldEnv,
+    ComplexDecisionEnv,
+    EarlyReturnEnv,
+    ComparisonChainEnv,
+)
 from .agent import DQNAgent
-from zrth import Wire, DType, IType, Term, Module
 
 __all__ = [
-    "Module",
     "SimpleQNet",
     "GridWorldQNet",
     "SimpleEnv",
@@ -16,9 +19,4 @@ __all__ = [
     "EarlyReturnEnv",
     "ComparisonChainEnv",
     "DQNAgent",
-    "Wire",
-    "DType",
-    "IType",
-    "Term",
-    "ExtendedModule",
 ]
