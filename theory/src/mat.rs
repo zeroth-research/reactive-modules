3d827af7858cf2ca658522c565006677eef2ef02
Marek Chalupa <marek.chalupa@zeroth.org>
2026-04-09 16:06:05 +0200

    theory: implement matrices
    Change the macros to be able to plug-in existing operations
    into theories.
    Then, implement the matrices in the obvious way.

--- theory/src/mat.rs
+++ theory/src/mat.rs
@@ -1,1 +1,32 @@
+use std::marker::PhantomData;
 
+use crate::*;
+
+/// Bitvector type parametrized by its size
+#[derive(Clone)]
+pub struct Mat<T: Type, const M: usize, const N: usize>(pub std::marker::PhantomData<T>);
+impl<T: Type, const M: usize, const N: usize> Type for Mat<T, M, N> {}
+
+#[derive(Copy, Clone)]
+pub struct MatMul<T: Type, const A: usize, const B: usize, const C: usize> {
+    pub t: PhantomData<T>,
+}
+impl<T: Type + Copy, const A: usize, const B: usize, const C: usize> Operation
+    for MatMul<T, A, B, C>
+{
+}
+
+impl<T: Type + Copy, const A: usize, const B: usize, const C: usize>
+    Operation2To1<Mat<T, A, B>, Mat<T, B, C>, Mat<T, A, C>> for MatMul<T, A, B, C>
+{
+}
+
+mk_theory!(
+    [T: Type, const M: usize, const N: usize]
+    Types(Mat => Mat<T, M, N>),
+    Add(Mat<T, M, N>, Mat<T, M, N>) => Mat<T, M, N>,
+    Id(Mat<T, M, N>) => Mat<T, M, N>
+    ;
+    [T: Type + Copy, const A: usize, const B: usize, const C: usize]
+    MatMul => MatMul<T, A, B, C>
+);
