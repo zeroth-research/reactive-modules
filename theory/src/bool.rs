b3013ab25aab47c59c41f64a694f952000b9b676
Marek Chalupa <marek.chalupa@zeroth.org>
2026-04-21 11:56:47 +0200

    theory: implement LIA

--- theory/src/bool.rs
+++ theory/src/bool.rs
@@ -12,13 +12,14 @@ TODO: add description
 
 use crate::*;
 
+#[derive(Clone, Copy, PartialEq)]
 pub enum PropDType {
-    Bool,
+    Bool(usize, usize),
 }
 
-/// TODO: write "formal" semantics
+#[derive(Clone, PartialEq, Debug)]
 pub enum Prop {
-    Const(bool),
+    Const(Vec<Vec<bool>>),
     And,
     Or,
     Xor,
@@ -29,12 +30,83 @@ pub enum Prop {
 impl Theory for Prop {
     type DType = PropDType;
 
-    fn _check(&self, read: &[Self::DType], write: &[Self::DType]) -> bool {
+    fn _check(&self, read: &[Self::DType], write: &[Self::DType]) -> Result<(), String> {
         match self {
-            Prop::Const(_) => return read.len() == 0 && write.len() == 1,
-            Prop::Not | Prop::Id => return read.len() == 1 && write.len() == 1,
-            Prop::Xor => return read.len() == 2 && write.len() == 1,
-            _ => return write.len() == 1,
+            Prop::Const(cm) => {
+                if read.len() > 0 {
+                    return Err("Const: cannot read values".into());
+                }
+                if write.len() != 1 {
+                    return Err("Const: must return a single value".into());
+                }
+                match &write[0] {
+                    PropDType::Bool(i, j) => {
+                        if cm.len() != *i {
+                            return Err(format!(
+                                "Const: Initializer has a wrong number of rows (has {}, expected {})",
+                                cm.len(),
+                                *i
+                            ));
+                        }
+                        if cm.iter().any(|row| row.len() != *j) {
+                            return Err(format!(
+                                "Const: some column of initializer has wrong dimension, expected {}",
+                                *j
+                            ));
+                        }
+                        Ok(())
+                    }
+                }
+            }
+            Prop::Not | Prop::Id => {
+                if read.len() != 1 {
+                    return Err(format!(
+                        "{:?}: must read a single value, got {}",
+                        self,
+                        read.len()
+                    ));
+                }
+                if write.len() != 1 {
+                    return Err(format!(
+                        "{:?}: must write a single value, got {}",
+                        self,
+                        write.len()
+                    ));
+                }
+                if write[0] != read[0] {
+                    return Err(format!(
+                        "{:?}: input and output type must be the same",
+                        self
+                    ));
+                }
+                Ok(())
+            }
+            Prop::And | Prop::Or | Prop::Xor => {
+                if read.len() != 2 {
+                    return Err(format!(
+                        "{:?}: must read two values, got {}",
+                        self,
+                        read.len()
+                    ));
+                }
+                if write.len() != 1 {
+                    return Err(format!(
+                        "{:?}: must write a single value, got {}",
+                        self,
+                        write.len()
+                    ));
+                }
+                if read[0] != read[1] {
+                    return Err(format!("{:?}: input values must have the same type", self));
+                }
+                if write[0] != read[1] {
+                    return Err(format!(
+                        "{:?}: input and output values must have the same type",
+                        self
+                    ));
+                }
+                Ok(())
+            }
         }
     }
 }
--- theory/src/lib.rs
+++ theory/src/lib.rs
@@ -1,23 +1,27 @@
 pub mod bool;
 pub mod bv;
 pub mod int;
-pub mod mat;
-pub mod nat;
+pub mod lia;
 pub mod real;
 
-trait Theory {
+pub trait Theory {
     type DType;
 
-    fn _check(&self, read: &[Self::DType], write: &[Self::DType]) -> bool;
+    fn _check(&self, read: &[Self::DType], write: &[Self::DType]) -> Result<(), String>;
 
-    fn check<T>(&self, read: &[T], write: &[T]) -> bool
+    fn check<T>(&self, read: &[T], write: &[T]) -> Result<(), String>
     where
-        T: TryInto<Self::DType>,
+        T: TryInto<Self::DType> + Clone,
     {
-        if let (r, w) = (read.try_into()?, write.try_into()?) {
-            return self._check(r, w);
+        // XXX: we have to translate to Vec here, which I'd like to avoid. But using iterators
+        // makes `check` harder. It probably makes sense to do that, but let it be the step 2.
+        let rd: Result<Vec<Self::DType>, _> = read.iter().cloned().map(TryInto::try_into).collect();
+        let wd: Result<Vec<Self::DType>, _> =
+            write.iter().cloned().map(TryInto::try_into).collect();
+        if let (Ok(r), Ok(w)) = (rd, wd) {
+            return self._check(r.as_slice(), w.as_slice());
         }
 
-        false
+        Err("Arguments have types from incompatible theory".into())
     }
 }
